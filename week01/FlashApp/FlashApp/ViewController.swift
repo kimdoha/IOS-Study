8ae1e39c7bc5c59f123b15a2e03be7f7fce38e95
Minji Kim <kkhhjj3393@gmail.com>
2021-06-14 00:08:55 +0900

    FlashApp

--- week01/FlashApp/FlashApp/ViewController.swift
+++ week01/FlashApp/FlashApp/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var label: UILabel!
     
     override func viewDidLoad() {
@@ -16,9 +17,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func buttonDidClick(_ sender: Any) {
-        label.text = "CLICK!"
+    @IBAction func onButtonClicked(_ sender: Any) {
+        view.backgroundColor = UIColor(red: 0.96, green: 0.78, blue: 0.78, alpha: 1.00)
+        label.textColor = UIColor.black
+        imageView.image = UIImage(systemName: "flashlight.on.fill")
     }
     
+    @IBAction func offButtonClicked(_ sender: Any) {
+        view.backgroundColor = UIColor(red: 0.13, green: 0.13, blue: 0.39, alpha: 1.00)
+        label.textColor = UIColor.white
+        imageView.image = UIImage(systemName: "flashlight.off.fill")
+    }
 }
 
