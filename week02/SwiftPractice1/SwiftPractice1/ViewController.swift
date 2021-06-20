d40f40fadd745cd67f3500f3ffad4f0effa44127
minnji88 <kkhhjj3393@gmail.com>
2021-06-21 00:00:44 +0900

    RamenApp

--- week02/SwiftPractice1/SwiftPractice1/ViewController.swift
+++ week02/SwiftPractice1/SwiftPractice1/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
 
+    @IBOutlet weak var onOffSwitch: UISwitch!
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
@@ -17,30 +18,22 @@ class ViewController: UIViewController {
 
     }
 
-    var isBackSide = true
 
-
-    @IBAction func buttonClicked(_ sender: Any) {
-        UIView.transition(with: imageView, duration: 0.6, options: .transitionFlipFromLeft){
-            
-        } completion: {
-            (Bool) in
-        }
+    @IBAction func onOffSwitchValueChanged(_ sender: Any) {
         
         UIView.transition(with: imageView, duration: 0.6, options: .transitionFlipFromLeft){
-            if self.isBackSide {
+            if self.onOffSwitch.isOn {
                 self.imageView.image = UIImage(named: "ace")
             } else {
                 self.imageView.image = UIImage(named: "poker")
             }
             
         } completion:{ (animated) in
-            self.isBackSide = !self.isBackSide // true | false 를 뒤집어줍니다
+
         }
         
-    
-        
     }
+    
     
 }
 
